f92b6a76bf800dbfb48a962975ed88106ec1dfa3
Eden Li <eden.li@gmail.com>
2011-11-07 14:38:28 -0800

    fix index.js for 0.5+

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require("./build/default/hash_ring").HashRing;
+try {
+  module.exports = require("./build/default/hash_ring").HashRing;
+}
+catch(e) {
+  // update for v0.5.5+
+  module.exports = require("./build/Release/hash_ring").HashRing;
+}
